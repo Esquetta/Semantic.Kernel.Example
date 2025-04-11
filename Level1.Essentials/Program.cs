9ae375b6ba9f29189ca058e3f3feef87b7b9c994
Furkan <esquetta@gmail.com>
2025-04-11 20:10:11 +0300

    Add project files.

---
+++ Level1.Essentials/Program.cs
@@ -0,0 +1,18 @@
+﻿using Codeblaze.SemanticKernel.Connectors.Ollama;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.SemanticKernel;
+
+var builder = Kernel.CreateBuilder().AddOllamaChatCompletion("deepseek-r1:latest","http://localhost:11434");
+
+builder.Services.AddScoped<HttpClient>();
+var kernel = builder.Build();
+
+
+
+while (true)
+{
+    Console.WriteLine("Ask Question: ");
+    string input = Console.ReadLine();
+    var response = await kernel.InvokePromptAsync(input);
+    Console.WriteLine($"Answer: {response.ToString()}");
+}
