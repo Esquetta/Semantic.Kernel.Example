fbadf72d46eae6809408bf52d57db0ae2a1f7306
Furkan <esquetta@gmail.com>
2025-04-13 20:25:38 +0300

    Level.ChatHistory_Objects added.

---
+++ Level3.History_Objects/Program.cs
@@ -0,0 +1,26 @@
+﻿using Codeblaze.SemanticKernel.Connectors.Ollama;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.SemanticKernel;
+using Microsoft.SemanticKernel.ChatCompletion;
+
+var builder = Kernel.CreateBuilder().AddOllamaChatCompletion("deepseek-r1:latest", "http://localhost:11434");
+
+builder.Services.AddScoped<HttpClient>();
+var kernel = builder.Build();
+
+var history = new ChatHistory();
+
+history.AddUserMessage("Merhaba bu gün hava nasıl");
+
+var chatCompletionService = kernel.GetRequiredService<IChatCompletionService>();
+var response = await chatCompletionService.GetChatMessageContentsAsync(history);
+history.AddAssistantMessage(response.ToString());
+
+history.AddUserMessage("Peki hafta önümüzde ki hafta için tahminler nasıl?");
+var response2 = await chatCompletionService.GetChatMessageContentsAsync(history);
+history.AddAssistantMessage(response2.ToString());
+
+Console.WriteLine(response2.First().Content);
+
+Console.Read();
+
