6d5ed0ffe0942bf44bb262e04256e303af1ea30d
Furkan <esquetta@gmail.com>
2025-04-15 19:57:56 +0300

    Plugins example added.

---
+++ Level4.Plugins/CalculatorPlugin.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.SemanticKernel;
+using System.ComponentModel;
+
+namespace Level4.Plugins
+{
+    public class CalculatorPlugin
+    {
+        [KernelFunction("add")]
+        [Description("Its sums two numeric values.")]
+        [return: Description("Returns sum of numeric result")]
+        public int Add(int number1, int number2) => number1 + number2;
+    }
+}
---
+++ Level4.Plugins/Program.cs
@@ -0,0 +1,28 @@
+﻿
+
+using Codeblaze.SemanticKernel.Connectors.Ollama;
+using Level4.Plugins;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.SemanticKernel;
+
+var builder = Kernel.CreateBuilder().AddOllamaChatCompletion("deepseek-r1:latest", "http://localhost:11434");
+
+
+builder.Services.AddScoped<HttpClient>();
+
+var kernel = builder.Build();
+
+
+kernel.Plugins.AddFromType<CalculatorPlugin>();
+
+var arguments = new KernelArguments
+{
+    ["number1"] = 3,
+    ["number2"] = 5
+};
+
+
+var addResult = await kernel.InvokeAsync("CalculatorPlugin", "add", arguments);
+Console.WriteLine($"Result: {addResult}");
+
+
