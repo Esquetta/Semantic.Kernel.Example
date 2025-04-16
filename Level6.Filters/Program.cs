6d6505198338e8c81a740a5f3dfd01dd06e2be11
Furkan <esquetta@gmail.com>
2025-04-16 20:38:00 +0300

    Filters added.

---
+++ Level6.Filters/Program.cs
@@ -0,0 +1,18 @@
+﻿using Codeblaze.SemanticKernel.Connectors.Ollama;
+using Level6.Filters.Filters;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.SemanticKernel;
+
+var builder = Kernel.CreateBuilder().AddOllamaChatCompletion("deepseek-r1:latest", "http://localhost:11434");
+
+
+builder.Services.AddScoped<HttpClient>();
+
+builder.Services.AddSingleton<IFunctionInvocationFilter, LoggingFilter>();
+builder.Services.AddSingleton<IPromptRenderFilter, SafePromptFilter>();
+builder.Services.AddSingleton<IAutoFunctionInvocationFilter, EarlyTerminationFilter>();
+var kernel = builder.Build();
+
+kernel.FunctionInvocationFilters.Add(new LoggingFilter());
+kernel.PromptRenderFilters.Add(new SafePromptFilter());
+kernel.AutoFunctionInvocationFilters.Add(new EarlyTerminationFilter());
